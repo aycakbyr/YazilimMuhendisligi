75c9cf43ff124bfd4a559f0e1d694e9cfa03f713
ayça <zeynepaycaakbayirr@gmail.com>
2025-03-20 23:05:22 +0300

    Gün 4: Django Rest Framework (DRF) ile API altyapısını hazırla.

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_lenght=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
