3815b82c9848823ecbf1ca4d0da0e12c0b85eb57
ayça <zeynepaycaakbayirr@gmail.com>
2025-03-27 04:04:24 +0300

    Gün 9: babel.config.json ve webpack.config.js dosyaları tamamlandı.

---
+++ music_controller/frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // <-- Değiştirildi
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // <-- Değiştirildi
+root.render(<App />);
